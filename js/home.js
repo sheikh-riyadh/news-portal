ba6fb419b51e7143604eb735e953006a7039b541
Sheikh Riyadh <sheikhriyad350883@gmail.com>
2022-09-03 16:08:08 +0600

    added new js file

--- js/home.js
+++ js/home.js
@@ -1,4 +1,3 @@
-
 /*========================================== 
 Show default news items on home page
 ============================================ */
@@ -49,7 +48,7 @@ const showDefaultItems = (datas) => {
                             </div>
                             <!-- Button -->
                             <div>
-                                <div class="btn btn-primary">Details</div>
+                                <div onclick="itemDetail()" class="btn btn-primary">Details</div>
                             </div>
                         </div>
                     </div>
@@ -62,36 +61,3 @@ const showDefaultItems = (datas) => {
 }
 
 loadDefaultItems()
-
-/*===========================================
-Show items when user click news items 
-=============================================*/
-const categories = async () => {
-    const res = await fetch('https://openapi.programming-hero.com/api/news/categories')
-    const data = await res.json();
-    dispplayCategories(data.data.news_category)
-    /* console.log(data.data.news_category) */
-}
-
-const dispplayCategories = (categories) => {
-    /* Categories container */
-    const categoriesItemsContainer = document.getElementById('items-container');
-
-    for (const category of categories) {
-        /* Create li */
-        const li = document.createElement('li');
-        /* Added class */
-        li.classList.add('nav-item')
-        li.innerHTML = `
-        <a onclick="showCategory('${category.category_id}')" class ="text-decoration-none p-2 fs-5">${category.category_name}</a>
-      `;
-        categoriesItemsContainer.appendChild(li)
-
-    }
-}
-
-
-const showCategory = (id) => {
-    console.log(id)
-}
-categories()
